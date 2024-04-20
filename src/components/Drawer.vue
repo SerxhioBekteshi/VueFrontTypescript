7abf73596fe80a7227c1ec44e8f5dcfa152786fa
SerxhioBekteshi <serxhio131@gmail.com>
2024-04-20 22:52:06 +0200

    genereal fixes

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <div class="card flex justify-content-center"> -->
-  <OnClickOutside
+  <!-- <OnClickOutside
     :options="{
       ignore: [
         // '.p-dialog p-component',
@@ -16,39 +16,39 @@
         }
       }
     "
+  > -->
+  <Sidebar
+    style="position: relative"
+    :visible="openDrawer"
+    position="right"
+    class="w-full md:w-30rem"
   >
-    <Sidebar
-      style="position: relative"
-      :visible="openDrawer"
-      position="right"
-      class="w-full md:w-30rem"
-    >
-      <template #header>
-        <div class="flex">
-          <h2>{{ title }}</h2>
-        </div>
-      </template>
-      <template #closeicon>
-        <i
-          @click="handleDrawerClose"
-          class="pi pi-times"
-          style="font-size: 1rem"
-        ></i>
-      </template>
-      <div class="body">
-        <!-- <ScrollPanel style="width: 100%; height: 100%"> -->
-        <slot></slot>
-        <!-- </ScrollPanel> -->
+    <template #header>
+      <div class="flex">
+        <h2>{{ title }}</h2>
       </div>
-      <div class="actions">
-        <div class="actionsAlign">
-          <div v-for="(action, index) in actions" :key="index">
-            <component :is="action.component" v-bind="action.props"></component>
-          </div>
+    </template>
+    <template #closeicon>
+      <i
+        @click="handleDrawerClose"
+        class="pi pi-times"
+        style="font-size: 1rem"
+      ></i>
+    </template>
+    <div class="body">
+      <!-- <ScrollPanel style="width: 100%; height: 100%"> -->
+      <slot></slot>
+      <!-- </ScrollPanel> -->
+    </div>
+    <div class="actions">
+      <div class="actionsAlign">
+        <div v-for="(action, index) in actions" :key="index">
+          <component :is="action.component" v-bind="action.props"></component>
         </div>
       </div>
-    </Sidebar>
-  </OnClickOutside>
+    </div>
+  </Sidebar>
+  <!-- </OnClickOutside> -->
 
   <!-- </div> -->
 </template>
